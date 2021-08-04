1809cadf4ffb2675b741de5d003bee7996c71c30
Elena Sharipova <elena.sh.r@yandex.ru>
2021-08-04 13:37:12 +0300

    Project 14. Edition 3

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const isEmail = require('validator/lib/isEmail');
 const bcrypt = require('bcryptjs');
+const ForbiddenError = require('../errors/forbidden-error');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -47,13 +48,13 @@ userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
+        return Promise.reject(new ForbiddenError('Неправильные почта или пароль'));
       }
 
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new Error('Неправильные почта или пароль'));
+            return Promise.reject(new ForbiddenError('Неправильные почта или пароль'));
           }
 
           return user;
